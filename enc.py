49c3cd2b230f8f1e15a2c93b477112292e2f5745
themobilehacker <75713786+themobilehacker@users.noreply.github.com>
2021-01-24 14:36:40 +0800

    Update enc.py

--- enc.py
+++ enc.py
@@ -1,5 +1,6 @@
-import sys,base64,time
+import sys,base64,time,hashlib
 from cryptography.fernet import Fernet
+#variables
 f=Fernet
 intr='''---------------------------
 --Tsociety file encrypter--
@@ -8,6 +9,7 @@ intr='''---------------------------
 99)exit
 ---------------------------
 >'''
+#gets input to turn into 32 url safe characters and convert it into md5 then byte like objects to use as fernet kney
 def key():
     print('')
     key=input('password>')
@@ -15,69 +17,25 @@ def key():
     if keyc<=7:
         print('password must have at least 8 characters')
         sys.exit()
-    elif keyc==8:
-        key=key+'abcdefghijklmnopqrstuvwx'
-    elif keyc==9:
-        key=key+'abcdefghijklmnopqrstuvw'
-    elif keyc==10:
-        key=key+'abcdefghijklmnopqrstuv'
-    elif keyc==11:
-        key=key+'abcdefghijklmnopqrstu'
-    elif keyc==12:
-        key=key+'abcdefghijklmnopqrst'
-    elif keyc==13:
-        key=key+'abcdefghijklmnopqrs'
-    elif keyc==14:
-        key=key+'abcdefghijklmnopqr'
-    elif keyc==15:
-        key=key+'abcdefghijklmnopq'
-    elif keyc==16:
-        key=key+'abcdefghijklmnop'
-    elif keyc==17:
-        key=key+'abcdefghijklmno'
-    elif keyc==18:
-        key=key+'abcdefghijklmn'
-    elif keyc==19:
-        key=key+'abcdefghijklm'
-    elif keyc==20:
-        key=key+'abcdefghijkl'
-    elif keyc==21:
-        key=key+'abcdefghijk'
-    elif keyc==22:
-        key=key+'abcdefghij'
-    elif keyc==23:
-        key=key+'abcdefghi'
-    elif keyc==24:
-        key=key+'abcdefgh'
-    elif keyc==25:
-        key=key+'abcdefg'
-    elif keyc==26:
-        key=key+'abcdef'
-    elif keyc==27:
-        key=key+'abcde'
-    elif keyc==28:
-        key=key+'abcd'
-    elif keyc==29:
-        key=key+'abc'
-    elif keyc==30:
-        key=key+'ab'
-    elif keyc==31:
-        key=key+'a'
     elif keyc>=33:
         print('password must be less than 33 characters')
         sys.exit()
-    key=base64.urlsafe_b64encode(bytes(key,'utf-8'))
+    key=hashlib.md5(str(key).encode())
+    key=key.hexdigest()
+    print(str(key))
+    key=base64.urlsafe_b64encode(bytes(str(key),'utf-8'))
     return(key)
+#uses the given key to encrypt a file with the given filename
 def enc(key):
     print('')
     fi=input('filename>')
     print('')
     print('   ***reading file data...***')
     fd=open(fi,'rb').read()
-    time.sleep(0.2)
+    time.sleep(0.3)
     print('')
     print('***initializing Fernet key...***')
-    time.sleep(0.2)
+    time.sleep(0.4)
     print('')
     print('   ***encrypting file data***')
     print('')
@@ -85,6 +43,7 @@ def enc(key):
     open(fi,'wb').write(en)
     print('      ***file encrypted***')
     print('')
+#uses the given key to decrypt a file with the given filename
 def dec(key):
     print('')
     fi=input('filename>')
@@ -94,7 +53,7 @@ def dec(key):
     time.sleep(0.2)
     print('')
     print('***initializing Fernet key...***')
-    time.sleep(0.2)
+    time.sleep(0.35)
     print('')
     print('    ***decrypting data...***')
     print('')
@@ -102,6 +61,7 @@ def dec(key):
     open(fi,'wb').write(de)
     print('      ***file decrypted***')
     print('')
+#run
 while True:
     c=input(intr)
     if c=='1':
