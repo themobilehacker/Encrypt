e3f2e191ca86171fbd5866eac6390d3bc011ebd8
themobilehacker <75713786+themobilehacker@users.noreply.github.com>
2021-01-30 05:55:57 +0000

    Update enc.py

--- enc.py
+++ enc.py
@@ -1,4 +1,4 @@
-import sys,base64,time,hashlib
+import sys,base64,time,hashlib,os
 from cryptography.fernet import Fernet
 #variables
 f=Fernet
@@ -31,17 +31,20 @@ def enc(key):
     fi=input('filename>')
     print('')
     print('   ***reading file data...***')
-    fd=open(fi,'rb').read()
+    os.system('zip enc -r %s'%fi)
+    fd=open('enc.zip','rb').read()
     time.sleep(0.3)
     print('')
     print('***initializing Fernet key...***')
     time.sleep(0.4)
     print('')
+    os.system('rm -rf %s'%fi)
     print('   ***encrypting file data***')
     print('')
     en=f(key).encrypt(fd)
-    open(fi,'wb').write(en)
+    open(fi+'.enc','wb').write(en)
     print('      ***file encrypted***')
+    os.system('rm -rf enc.zip')
     print('')
 #uses the given key to decrypt a file with the given filename
 def dec(key):
@@ -58,8 +61,11 @@ def dec(key):
     print('    ***decrypting data...***')
     print('')
     de=f(key).decrypt(fd)
-    open(fi,'wb').write(de)
+    open(fi.replace('.enc','.zip'),'wb').write(de)
+    os.system('unzip %s'%fi.replace('.enc',''))
     print('      ***file decrypted***')
+    os.system('rm -rf %s'%fi.replace('.enc','.zip'))
+    os.system('rm -rf %s'%fi)
     print('')
 #run
 while True:
