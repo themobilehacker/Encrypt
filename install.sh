991b740ac7c0adc4d66c95a4de397c01280849a8
themobilehacker <75713786+themobilehacker@users.noreply.github.com>
2021-03-20 16:20:23 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,8 +12,11 @@ echo '
             \#\___/#/
              \#####/
 '
-chmod +x enc.py
-ENC="alias pycrypt='python3 ${PWD}/enc.py'"
+echo 'installing pycrypt...'
+cp enc.py ~/.pycrypt
+ENC="alias pycrypt='python3 ~/.pycrypt/enc.py'"
 echo $ENC>>~/.bashrc
 echo $ENC>>~/.zshrc
 echo $ENC>>~/.zprofile
+echo 'pycrypt installed.'
+echo 'run by typing pycrypt -e or -d [directory]'
